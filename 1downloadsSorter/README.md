43a5f49b8a05b5fe606ac41cbbcea6439acd487d
docherak <docherak@protonmail.ch>
2022-06-01 01:26:48 +0200

    Init files
    Added a shell script that manages ones Downloads folder, including instructions on how to fully automate using crontab.

---
+++ 1downloadsSorter/README.md
@@ -0,0 +1,29 @@
+# Downloads Manager
+The following is a bash script I use to manage my Downloads folder.
+
+It scans for any changes in the Downloads folder (excl. subfolders) and sorts the files by scanning their extensions, moving them to the specified folders accordingly.
+
+Works with files that include spaces and special characters.
+
+Tested with [zsh](https://www.zsh.org/).
+
+**Prerequisites:**
+
+```
+sudo apt-get install inotify-tools
+```
+
+**Usage:**
+
+```
+chmod a+x downloadsScanner.sh
+```
+
+**How I launch this on reboot:**
+
+```
+crontab -e
+
+// add the following line:
+@reboot [path to shell script] & disown
+```
---
+++ 1downloadsSorter/downloadsScanner.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+search_dir=/home/$USER/Downloads
+
+# Quick access in case of renaming folders
+# Downloads folder:
+DOKUMENTY=Docs
+KOMPRIM=Archives
+EXECUTABLES=Execs
+PYTHON=Code/Python
+# Home folder:
+PICS=Pictures
+
+# Checking loop
+while inotifywait -e modify,create,delete,move $search_dir
+do
+
+	# prereq: inotify-tools
+	
+	for file in ${search_dir}/*
+	do
+		filename=$(basename "$file")
+		
+		if [[ ($filename == *.txt*) ||
+			($filename == *.doc*) ||
+			($filename == *.pdf*) ||
+			($filename == *.xlsx*) ]]
+		then
+			mv -n $search_dir/"$filename" /home/$USER/Downloads/$DOKUMENTY/"$filename"  
+		
+		elif [[ ($filename == *.zip*) ||
+			($filename == *.tar*) ||
+			($filename == *.gz*) ||
+		        ($filename == *.br*) ||
+			($filename == *.bz*) ||
+			($filename == *.xz*) ||
+			($filename == *.zst*) ]]
+		then
+			mv -n $search_dir/"$filename" /home/$USER/Downloads/$KOMPRIM/"$filename"  
+		
+		elif [[ ($filename == *.deb*) ||
+			($filename == *.sh*) ||
+			($filename == *.AppImage*) ||
+			($filename == *.run*) ]]
+		then
+			mv -n $search_dir/"$filename" /home/$USER/Downloads/$EXECUTABLES/"$filename"  
+		
+		elif [[ ($filename == *.jpg*) ||
+			($filename == *.png*) ||
+			($filename == *.gif*) ||
+			($filename == *.jpeg*) ||
+			($filename == *.svg*) ]]
+		then
+			mv -n $search_dir/"$filename" /home/$USER/$PICS/"$filename"
+		
+		elif [[ ($filename == *.ipynb*) ||
+			($filename == *.py*) ]]
+		then
+			mv -n $search_dir/"$filename" /home/$USER/Downloads/$PYTHON/"$filename"
+		fi
+	done
+
+done
